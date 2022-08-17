340221fcb47f4ff20e66a1ad31c3795b22e0c7eb
IgorKhryukin <i.a.khryukin@gmail.com>
2022-08-18 00:31:06 +0700

    Add tests for Horse class

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +12,17 @@ class HorseTest {
         assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10.5, 10.0));
     }
 
+    @Test
+    public void checkThrowMessageWhenFirstParameterIsNullInConstructor(){
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> new Horse(null, 10.5, 10.0));
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "\t\t\t", "\n\n\n"})
+    public void getThrowWhenHorseNameIsEmptyInConstructor(String name){
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, 10.5, 10.0));
+    }
 
 }
